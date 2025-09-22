3ffc20bee30daa00722414c0c518b5abad920654
FelixChaos <felixchaos@qq.com>
2025-09-22 23:54:12 +0800

    忽略 tools/ update/ database/CustomMissions/；更新 README 为 Manaka MOD Station 项目

--- tools/test_import_update_checker.py
+++
@@ -1,9 +0,0 @@
-# small test runner to import update_checker
-import importlib.util
-from pathlib import Path
-spec = importlib.util.spec_from_file_location('update_checker', str(Path(__file__).resolve().parents[1] / 'src' / 'update_checker.py'))
-module = importlib.util.module_from_spec(spec)
-import sys
-sys.modules[spec.name] = module
-spec.loader.exec_module(module)
-print('IMPORT_OK')
